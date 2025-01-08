a803e21a6f46968ecc84a16a56e5cbc04356c9bb
mrcoolmc <ramirezmoraleivan21@gmail.com>
2025-01-07 21:02:25 -0800

    Update mkey.bat

--- mkey.bat
+++ mkey.bat
@@ -3,5 +3,5 @@ rem replace 12 with the 3ds's month number
 rem replace 25 with the 3ds's day number
 rem then double click this .bat
 
-python mkey.py -m 12 -d 25 0123456789 CTR
+python mkey.py -m 1 -d 14 1439843698 CTR
 pause
